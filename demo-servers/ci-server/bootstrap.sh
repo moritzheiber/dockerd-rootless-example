f2061750dc178884c4d70ec581874a3e1d43d0dc
Philipp Gerhard <philipp.gerhard@thoughtworks.com>
2022-04-22 16:28:19 +0200

    Created Jenkins server using Vagrant. Created pipeline that modifies file on host system using root priviledges

---
+++ demo-servers/ci-server/bootstrap.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# Install Docker
+apt-get update
+apt-get -y install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+apt-get -y install docker-ce docker-ce-cli containerd.io
+
+usermod -aG docker vagrant
+newgrp docker
+
+# Install Docker Compose
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+# Install Jenkins
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key \
+  | tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ \
+  | tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+apt-get update
+
+sudo apt-get -y install openjdk-11-jre
+sudo apt-get -y install jenkins
+
+echo "Completed provisioning"
